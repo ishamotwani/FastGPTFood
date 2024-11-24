88f3d966ada9343f6f828590b78d59128f74773d
Toad <111982929+BestToad57@users.noreply.github.com>
2024-11-24 13:42:00 -0500

    Simple Getting Pages Down

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FastGPTFood
  Website Dev
+
+ Please Put Your Name Here:
+ Ethan Ayube
+
+
+ I don't know what else to put in a read me
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+//Import Section
+import {BrowserRouter as Router, Route, Routes, Link, useLocation } from  'react-router-dom';
 import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home.jsx'
+import Order from './pages/Order'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
+      <Routes>
+
+      </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
