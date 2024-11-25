6f2445b68c819c1e9f5569b44ecfdd431417f45d
Toad <111982929+BestToad57@users.noreply.github.com>
2024-11-25 09:50:46 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate } from 'react-router-dom';
-import TempImg from './images/TempImg.png';
+import TempImg from '../images/TempImg.png';
+import Navigation from '../functions/Navigation';
 
 //Order Button & the TempImg stuff has not been tested
 
